1ea8b2a77e8f9974b8957b92ab07f28879670dda
EliasAlvial <72842792+EliasAlvial@users.noreply.github.com>
2021-01-27 22:28:27 -0500

    Update Main.java
    added my name to new branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        
+        System.out.println("Elias Alvial");
 
         SolarSystem FactorX =new SolarSystem();
         FactorX.Sun=1;
