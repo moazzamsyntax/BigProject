018e24e0007168751f2c1c03d87b67802aca6079
jackplantin <70772308+jackplantin@users.noreply.github.com>
2021-01-27 21:24:25 -0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Jack
 
         SolarSystem FactorX =new SolarSystem();
         FactorX.Sun=1;
@@ -15,6 +16,8 @@ public class Main {
         troyo.Heat="56748kj";
         troyo.temperature="347878k";
         troyo.Radius="99899898km";
+        
+        
 
     }
 }
